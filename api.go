b0f53acb95a9abf3c52759a3faabff11bb156740
Charles Francoise <charles.francoise@gmail.com>
2018-11-11 23:06:10 +0100

    ListRequests handler

--- api.go
+++ api.go
@@ -1,12 +1,18 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"time"
 
 	log "github.com/sirupsen/logrus"
 )
+
+type requestResponse struct {
+	Request  *Request  `json:"request"`
+	Response *Response `json:"response"`
+}
 
 type APIServer struct{}
 
@@ -20,15 +26,20 @@ func NewAPIHandler() http.Handler {
 func HandleListRequests(w http.ResponseWriter, req *http.Request) {
 	var err error
 
+	if req.Method != http.MethodGet {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+
 	startStr := req.FormValue("start")
 	var startTime time.Time
 	if len(startStr) == 0 {
 		startTime = time.Time{}
 	} else {
-		startTime, err = time.Parse(time.RFC3339, startStr)
+		startTime, err = time.Parse(time.RFC3339Nano, startStr)
 		if err != nil {
-			errMsg := fmt.Sprintf("invalid start time: %s", err)
-			http.Error(w, errMsg, http.StatusBadRequest)
+			fmt.Fprintf(w, "invalid start time")
+			w.WriteHeader(http.StatusBadRequest)
 			return
 		}
 	}
@@ -38,13 +49,37 @@ func HandleListRequests(w http.ResponseWriter, req *http.Request) {
 	if len(endStr) == 0 {
 		endTime = MaxUnixTime
 	} else {
-		endTime, err = time.Parse(time.RFC3339, endStr)
+		endTime, err = time.Parse(time.RFC3339Nano, endStr)
 		if err != nil {
-			errMsg := fmt.Sprintf("invalid end time: %s", err)
-			http.Error(w, errMsg, http.StatusBadRequest)
+			fmt.Fprintf(w, "invalid end time")
+			w.WriteHeader(http.StatusBadRequest)
 			return
 		}
 	}
 
-	log.Infof("ListRequests: start=%s,end=%s", startTime, endTime)
+	requests, responses, err := ListRequests(startTime, endTime)
+	if err != nil {
+		log.Errorf("couldn't get requests and responses: %s", err)
+		http.Error(w, "internal error", http.StatusInternalServerError)
+		return
+	}
+	if len(requests) != len(responses) {
+		log.Errorf("length mismatch: %d requests - %d responses", len(requests), len(responses))
+		http.Error(w, "internal error", http.StatusInternalServerError)
+		return
+	}
+	requestResponses := make([]requestResponse, len(requests))
+	for i := 0; i < len(requests); i++ {
+		requestResponses[i].Request = requests[i]
+		requestResponses[i].Response = responses[i]
+	}
+
+	b, err := json.Marshal(requestResponses)
+	if err != nil {
+		log.Errorf("couldn't marshal requests and responses: %s", err)
+		http.Error(w, "internal error", http.StatusInternalServerError)
+		return
+	}
+
+	w.Write(b)
 }
