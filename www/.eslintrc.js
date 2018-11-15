d5ee0974e9250725e8dd281c0fb452e4fda27e14
Charles Francoise <charles.francoise@gmail.com>
2018-11-15 23:32:10 +0100

    a bit of cleanup

--- www/.eslintrc.js
+++ www/.eslintrc.js
@@ -3,20 +3,21 @@ module.exports = {
   env: {
     node: true
   },
-  extends: ["plugin:vue/essential"],
+  extends: ['plugin:vue/essential'],
   rules: {
     // allow async-await
-    "generator-star-spacing": "off",
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
-    "semi": ["error", "never"],
-    "no-extra-semi": "error",
-    "quotes": ["error", "single"],
-    "sort-imports": "error",
-    "no-var": "error",
-    "prefer-const": "error"
+    'generator-star-spacing': 'off',
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'semi': ['error', 'never'],
+    'no-extra-semi': 'error',
+    'quotes': ['error', 'single'],
+    'sort-imports': 'error',
+    'no-var': 'error',
+    'prefer-const': 'error',
+    'sort-imports': 'off'
   },
   parserOptions: {
-    parser: "babel-eslint"
+    parser: 'babel-eslint'
   }
 };
