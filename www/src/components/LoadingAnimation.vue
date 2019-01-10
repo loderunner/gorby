40bae79600b76ef12d125e0d50b424e12c7fb6db
Charles Francoise <charles.francoise@gmail.com>
2019-01-10 14:33:56 +0100

    Use loading animation

---
+++ www/src/components/LoadingAnimation.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="loading-animation">
+    <div class="dot"></div>
+    <div class="dot" style="animation-delay: 0.2s"></div>
+    <div class="dot" style="animation-delay: 0.4s"></div>
+  </div>
+</template>
+<style lang="scss" scoped>
+.loading-animation {
+  &:before {
+    content: "\200b";
+  }
+
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+}
+
+@keyframes fade {
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+}
+
+@keyframes bounce {
+  50% {
+    transform: translateY(-3px);
+  }
+  60% {
+    transform: translateY(0px);
+  }
+}
+
+.dot {
+  background-color: black;
+
+  height: 6px;
+  width: 6px;
+  margin: 2px;
+  border-radius: 50%;
+
+  animation-name: fade, bounce;
+  animation-duration: 1s, 1s;
+  animation-timing-function: ease-in, linear;
+  animation-iteration-count: infinite, infinite;
+  animation-direction: alternate, normal;
+}
+</style>
--- www/src/components/RequestsTable.vue
+++ www/src/components/RequestsTable.vue
@@ -1,14 +1,16 @@
 <template>
   <b-table :data="requests" :selected.sync="selected" @click="rowSelected" narrowed hoverable>
     <template slot-scope="props">
-      <b-table-column field="id" label="ID" numeric>{{ props.row.id }}</b-table-column>
+      <!-- <b-table-column field="id" label="ID" numeric>{{ props.row.id }}</b-table-column> -->
       <b-table-column
         field="timestamp"
         label="Time"
       >{{ props.row.timestamp | moment('HH:mm:ss.SSS') }}</b-table-column>
-      <b-table-column field="method" label="Method">{{ props.row.method }}</b-table-column>
       <b-table-column field="host" label="Host">{{ props.row.host }}</b-table-column>
-      <b-table-column field="path" label="Path" width="6000">{{ props.row.path }}</b-table-column>
+      <b-table-column field="path" label="Path" width="70000">{{ props.row.path }}</b-table-column>
+      <b-table-column field="status" label="Status">
+        <LoadingAnimation/>
+      </b-table-column>
     </template>
   </b-table>
 </template>
@@ -16,12 +18,14 @@
 <script>
 import { Table } from 'buefy'
 import { mapGetters } from 'vuex'
+import LoadingAnimation from './LoadingAnimation'
 import Requests from '../store/modules/requests'
 
 export default {
   name: 'RequestsTable',
   components: {
-    Table
+    Table,
+    LoadingAnimation
   },
   computed: {
     ...mapGetters(['requests'])
