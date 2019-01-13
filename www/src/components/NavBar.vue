ea04165a37a7e046c20322bcf75e47d70dcfb294
Charles Francoise <charles.francoise@gmail.com>
2019-01-13 23:58:04 +0100

    add NavBar

---
+++ www/src/components/NavBar.vue
@@ -0,0 +1,25 @@
+<template>
+  <nav class="navbar is-fixed-top" role="navigation">
+    <div class="navbar-brand">
+      <h1 class="navbar-item title">Gorby</h1>
+    </div>
+    <div class="navbar-menu">
+      <div class="navbar-end">
+        <div class="navbar-item">
+          <div class="control has-icons-left">
+            <input id="searchInput" class="input" type="text" placeholder="Search">
+            <span class="icon is-small is-left">
+              <i class="fas fa-search"></i>
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<style lang="scss" scoped>
+#searchInput {
+  width: 300px;
+}
+</style>
