ecd2cffb94b49d78d4595180bdba948bb0e4ff1a
Charles Francoise <charles.francoise@gmail.com>
2019-02-10 23:15:32 +0100

    add watch npm task

--- www/.eslintrc.js
+++ www/.eslintrc.js
@@ -5,7 +5,6 @@ module.exports = {
   rules: {
     // allow async-await
     'generator-star-spacing': 'off',
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'semi': ['error', 'never'],
     'no-extra-semi': 'error',
